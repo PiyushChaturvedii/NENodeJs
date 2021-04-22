9e174a7bb975f4d5f86d91e40f3753c808d0488a
piyush-chaturvedi-au9 <piyush1.chatur@gmail.com>
2021-04-22 21:04:59 +0530

    done

--- 06.FirstAPI.js
+++ 06.FirstAPI.js
@@ -1,9 +1,19 @@
-// First API with HTTP module
-
+// First API with HTTP module.
 var http = require('http');
-var data = { name: "anil", age: "20", email:"anil@gmail.com" };
-http.createServer(function (req,res) {
-    res.writeHead(200,{'Content-Type':'application\json'});
+var data = [
+    {name:"anil",age:"25",email:"anil@gmail.com"},
+    {name:"sunil",age:"45",email:"sunil@gmail.com"},
+    {name:"peter",age:"55",email:"peter@gmail.com"},
+    {name:"mohan",age:"65",email:"mohan@gmail.com"},
+
+];
+
+
+
+http.createServer(function (req, res) {
+    res.writeHead(200,{'Content-Type':'application\json'})
     res.write(JSON.stringify(data));
-    res.end()
-}).listen(3900)
+    res.end();
+}).listen(3900);
+
+
---
+++ 07.FirstHtml.js
@@ -0,0 +1,15 @@
+// make HTML web page
+
+var http = require('http');
+var page ="
+<h1>Hello Node JS</h1> 
+<input type = 'text' /> <br/> <br/>
+<input type='text' /><br/><br/>
+<input type='text' />"
+http.createServer(function (req, res) {
+    res.writeHead(200,{'Content-Type':"text/html"});
+    res.write(page);
+    res.end();
+}).listen(3900);
+
+
