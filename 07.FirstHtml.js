025e569ed7ba4a1a5326d22a4cbd9d858c8778fc
piyush-chaturvedi-au9 <piyush1.chatur@gmail.com>
2021-04-23 19:25:49 +0530

    done

--- 07.FirstHtml.js
+++ 07.FirstHtml.js
@@ -1,11 +1,11 @@
 // make HTML web page
 
 var http = require('http');
-var page ="
+var page =`
 <h1>Hello Node JS</h1> 
 <input type = 'text' /> <br/> <br/>
 <input type='text' /><br/><br/>
-<input type='text' />"
+<input type='text' />`
 http.createServer(function (req, res) {
     res.writeHead(200,{'Content-Type':"text/html"});
     res.write(page);
